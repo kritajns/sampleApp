796788173c49c1d3998367f897f061d42664cb1d
Kritajna Shrestha <kritajna@esignature.com.np>
2024-01-09 17:42:41 +0545

    updated styles for product card

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-
-const ProductCard = ({title, price, imageURI}) => {
+const ProductCard = ({ title, price, imageURI, onPress }) => {
   return (
-    <View style={styles.productContainer}>
-        <Image source={{uri: imageURI}} style={styles.imageStyle} />
-      <Text style={styles.titleText} numberOfLines={2}>{title}</Text>
+    <TouchableOpacity style={styles.productContainer} onPress={onPress}>
+      <Image source={{ uri: imageURI }} style={styles.imageStyle} />
+      <Text style={styles.titleText} numberOfLines={2}>
+        {title}
+      </Text>
       <Text style={styles.textStyle}>$ {price}</Text>
-    </View>
-  )
-}
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    productContainer: {
-        flex: 1,
-        margin: 8,
-        padding: 10,
-        borderColor: 'grey',
-        borderWidth: 1,
-        borderRadius: 8,
-        height: 240,
-        backgroundColor: 'white'
-    },
-    titleText: {
-        fontSize: 16,
-        fontWeight: '500',
-        marginTop: 10,
-        marginBottom: 5,
-        color: 'grey',
-    },
-    textStyle: {
-        fontSize: 19,
-        fontWeight: 'bold',
-        // fontStyle: 'italic',
-        color: 'orange'
-    },
-    imageStyle: {
-        width: '100%',
-        height: '60%',
-        resizeMode: 'contain'
-    },
+  productContainer: {
+    flex: 1,
+    margin: 8,
+    padding: 10,
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 8,
+    height: 240,
+    backgroundColor: "white",
+  },
+  titleText: {
+    fontSize: 16,
+    fontWeight: "500",
+    marginTop: 10,
+    marginBottom: 5,
+    color: "grey",
+  },
+  textStyle: {
+    fontSize: 19,
+    fontWeight: "bold",
+    // fontStyle: 'italic',
+    color: "orange",
+  },
+  imageStyle: {
+    width: "100%",
+    height: "60%",
+    resizeMode: "contain",
+  },
 });
 
 export default ProductCard;
