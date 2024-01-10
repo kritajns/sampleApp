40c9115e11b10a8d9bbb00aab87a8b01afe3e86e
Kritajna Shrestha <kritajna@esignature.com.np>
2024-01-10 17:22:45 +0545

    added reducer for cart add & remove

--- app/redux/reducer.js
+++ app/redux/reducer.js
@@ -1,27 +1,67 @@
 const initialState = {
-    items: [],
-    count: 0,
-  };
-  
-  const cartReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_TO_CART':
+  items: [],
+  count: 0,
+};
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      const sameItem = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (sameItem) {
+        const updatedItems = state.items.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+
         return {
           ...state,
-          items: [...state.items, action.payload],
+          items: updatedItems,
           count: state.count + 1,
         };
+      } else {
+        return {
+          ...state,
+          items: [...state.items, { ...action.payload, quantity: 1 }],
+          count: state.count + 1,
+        };
+      }
 
-        case 'REMOVE_FROM_CART':
-            return {
-              ...state,
-              items: state.items.filter((item) => item.id !== action.payload),
-              count: state.count - 1,
-            };
-      
-      default:
-        return state;
-    }
-  };
-  
-  export default cartReducer;
+    case "REMOVE_FROM_CART":
+      const removeItem = state.items.find((item) => item.id === action.payload);
+
+      if (removeItem) {
+        if (removeItem.quantity > 1) {
+          const updatedItems = state.items.map((item) =>
+            item.id === action.payload
+              ? { ...item, quantity: item.quantity - 1 }
+              : item
+          );
+
+          return {
+            ...state,
+            items: updatedItems,
+            count: state.count - 1,
+          };
+        } else {
+          const updatedItems = state.items.filter(
+            (item) => item.id !== action.payload
+          );
+
+          return {
+            ...state,
+            items: updatedItems,
+            count: state.count - 1,
+          };
+        }
+      }
+
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
--- app/screens/CartScreen.js
+++ app/screens/CartScreen.js
@@ -1,47 +1,98 @@
 import React from "react";
-import {SafeAreaView, View, Text, FlatList, TouchableOpacity, Image, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
-import HeaderTop from "../components/HeaderTop";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { removeFromCart } from "../redux/Actions";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 const CartScreen = ({ navigation, route }) => {
+  const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.items);
-  console.log('this is cart items-=-=-', cartItems);
 
   // Calculate total price
-  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
 
-  const handleRemoveItem = () => {
-    console.log('item removed');
+  // to remove items from cart
+  const handleRemoveItem = (id) => {
+    console.log("item removed");
+    dispatch(removeFromCart(id));
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <HeaderTop title={'My Carts'} /> */}
-      <Text style={{fontWeight: 'bold', fontSize: 18}}>My Carts</Text>
-      <FlatList
-        data={cartItems}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.cartContainer}>
-            <View style={{marginLeft: 20}}>
-              <Image source={{uri: item?.image}} style={styles.image} />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          marginBottom: 10,
+        }}
+      >
+        <Icon name="arrow-left" size={30} onPress={() => navigation.goBack()} />
+        <View style={{ alignItems: "center", width: 350 }}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+            }}
+          >
+            My Carts
+          </Text>
+        </View>
+      </View>
+      {cartItems.length == 0 ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text style={{ fontSize: 16, fontWeight: "500" }}>
+            No cart items found.
+          </Text>
+        </View>
+      ) : (
+        <FlatList
+          data={cartItems}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.cartContainer}>
+              <View style={styles.imageContainer}>
+                <Image source={{ uri: item?.image }} style={styles.image} />
+              </View>
+              <View style={styles.textContainer}>
+                <Text numberOfLines={2} style={styles.headerText}>
+                  {item.title}
+                </Text>
+                <Text style={styles.textStyle}>Quantity: {item.quantity}</Text>
+                <Text style={styles.textStyle}>Price: ${item.price}</Text>
+                <Text
+                  style={[
+                    styles.textStyle,
+                    { fontWeight: "bold", color: "green", fontSize: 16 },
+                  ]}
+                >
+                  Total: ${item.price * item.quantity}
+                </Text>
+              </View>
+              <TouchableOpacity onPress={() => handleRemoveItem(item.id)}>
+                <Icon name="delete" size={30} color="red" />
+              </TouchableOpacity>
             </View>
-            <View style={styles.textContainer}>
-              <Text numberOfLines={2}>{item.title}</Text>
-              <Text>Quantity: 1</Text>
-              <Text>Price: ${item.price}</Text>
-              <Text>Total: ${item.price * 1}</Text>
-            </View>
-            <TouchableOpacity onPress={() => handleRemoveItem(item.id)}>
-              <Text>Remove</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      />
-      
+          )}
+        />
+      )}
+
       {cartItems.length > 0 && (
-        <View>
-          <Text>Total Price: ${totalPrice}</Text>
+        <View style={styles.totalContainer}>
+          <Text style={styles.totalText}>Total Price: ${totalPrice}</Text>
         </View>
       )}
     </SafeAreaView>
@@ -51,23 +102,57 @@ const CartScreen = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 20,
+    margin: 10,
     padding: 10,
     marginTop: 30,
+    backgroundColor: "white",
   },
   cartContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems:'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "lightgrey",
+    borderRadius: 10,
+    marginVertical: 5,
+    padding: 10,
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
   image: {
-    width: 80,
-    height: 80,
-    resizeMode: 'contain'
+    width: 90,
+    height: 90,
+    resizeMode: "contain",
+    borderRadius: 8,
   },
   textContainer: {
-    width: 300,
-    marginHorizontal: 10
+    flex: 1,
+    marginLeft: 10,
+  },
+  headerText: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  textStyle: {
+    marginTop: 5,
+    fontSize: 14,
+  },
+  totalContainer: {
+    margin: 10,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+    borderRadius: 10,
+  },
+  totalText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
 
