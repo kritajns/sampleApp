b7cde63471e8e49def5510bc9668b1cae82b9271
Kritajna Shrestha <kritajna@esignature.com.np>
2024-01-10 17:22:18 +0545

    added message for view

--- app/screens/MyProduct.js
+++ app/screens/MyProduct.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -11,9 +11,9 @@ import SingleProduct from "../components/SingleProduct";
 import HeaderTop from "../components/HeaderTop";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../redux/Actions";
+import { showMessage } from "react-native-flash-message";
 
 const MyProduct = ({ navigation, route }) => {
-
   const newProduct = route?.params?.data;
   const dispatch = useDispatch();
 
@@ -21,8 +21,7 @@ const MyProduct = ({ navigation, route }) => {
   const [cartItems, setCartItems] = useState([]);
 
   const cartState = useSelector((state) => state);
-  console.log('testing-cart-state', cartState);
-
+  console.log("testing-cart-state", cartState);
 
   // function to check if the item is in the cart
   const cartItemCheck = () => {
@@ -32,41 +31,61 @@ const MyProduct = ({ navigation, route }) => {
 
   // adding items to cart
   const handlePress = () => {
-    const isInCart = cartItemCheck();
+    // const isInCart = cartItemCheck();
+    // if (!isInCart) {
+    //   // setCartItems([...cartItems, newProduct]);
+    //   // setCartCount(cartCount + 1);
+    //   dispatch(addToCart(newProduct));
+    //   console.log("Item added to the cart");
+    // } else {
+    //   console.log("Item already in the cart");
+    // }
 
-    if (!isInCart) {
-      // setCartItems([...cartItems, newProduct]);
-      // setCartCount(cartCount + 1); 
-      dispatch(addToCart(newProduct));
-      console.log('Item added to the cart');
-    } else {
-      console.log('Item already in the cart');
-    }
+    dispatch(addToCart(newProduct));
+    console.log("Item added to the cart");
+    showMessage({
+      message: "Item added to cart",
+      type: "success",
+    });
   };
-  console.log('cart-count', cartCount);
 
+  // navigating to cart screen
   const navigateToCart = () => {
-    navigation.navigate('MyCart');
+    navigation.navigate("MyCart");
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-      <HeaderTop
-        title={newProduct?.category}
-        rightIcon
-        // cartCount={cartCount}
-        onPress2={navigateToCart}
-      />
-      <SingleProduct
-        imageURL={newProduct?.image}
-        title={newProduct?.title}
-        price={newProduct?.price}
-        desc={newProduct?.description}
-      />
-      <TouchableOpacity style={styles.cartContainer} onPress={handlePress}>
-        <Text style={styles.cartText}>Add to Cart</Text>
-      </TouchableOpacity>
+        <HeaderTop
+          title={newProduct?.category}
+          rightIcon
+          // cartCount={cartCount}
+          onPress2={navigateToCart}
+        />
+        <SingleProduct
+          imageURL={newProduct?.image}
+          title={newProduct?.title}
+          price={newProduct?.price}
+          desc={newProduct?.description}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginHorizontal: 20,
+          }}
+        >
+          <TouchableOpacity style={styles.cartContainer} onPress={handlePress}>
+            <Text style={styles.cartText}>Add to Cart</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.cartContainer, { backgroundColor: "darkblue" }]}
+            onPress={navigateToCart}
+          >
+            <Text style={styles.cartText}>Go To Cart</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -83,17 +102,17 @@ const styles = StyleSheet.create({
   cartContainer: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: 'orange',
+    backgroundColor: "orange",
     padding: 10,
     marginHorizontal: 20,
     marginTop: 25,
     marginBottom: 10,
-    borderRadius: 10
+    borderRadius: 10,
   },
   cartText: {
     color: "white",
     fontWeight: "600",
-    fontSize: 16
+    fontSize: 16,
   },
 });
 
