dfc2b5225d7e83e823c5e74eefefe63192daf337
kritajns <kritajnshrest@gmail.com>
2024-01-10 07:55:14 +0545

    added to cart

--- app/screens/MyProduct.js
+++ app/screens/MyProduct.js
@@ -1,33 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   SafeAreaView,
+  ScrollView,
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import SingleProduct from "../components/SingleProduct";
+import HeaderTop from "../components/HeaderTop";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../redux/Actions";
 
 const MyProduct = ({ navigation, route }) => {
-  const newProduct = route?.params?.data;
 
+  const newProduct = route?.params?.data;
+  const dispatch = useDispatch();
+
+  const [cartCount, setCartCount] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+
+  const cartState = useSelector((state) => state);
+  console.log('testing-cart-state', cartState);
+
+
+  // function to check if the item is in the cart
+  const cartItemCheck = () => {
+    const isInCart = cartItems.some((item) => item.id === newProduct.id);
+    return isInCart;
+  };
+
+  // adding items to cart
   const handlePress = () => {
-    console.log("inside-press-cart");
+    const isInCart = cartItemCheck();
+
+    if (!isInCart) {
+      // setCartItems([...cartItems, newProduct]);
+      // setCartCount(cartCount + 1); 
+      dispatch(addToCart(newProduct));
+      console.log('Item added to the cart');
+    } else {
+      console.log('Item already in the cart');
+    }
+  };
+  console.log('cart-count', cartCount);
+
+  const navigateToCart = () => {
+    navigation.navigate('MyCart');
   };
 
   return (
     <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <HeaderTop
+        title={newProduct?.category}
+        rightIcon
+        // cartCount={cartCount}
+        onPress2={navigateToCart}
+      />
       <SingleProduct
         imageURL={newProduct?.image}
         title={newProduct?.title}
         price={newProduct?.price}
         desc={newProduct?.description}
-        category={newProduct?.category}
       />
       <TouchableOpacity style={styles.cartContainer} onPress={handlePress}>
         <Text style={styles.cartText}>Add to Cart</Text>
       </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -36,17 +76,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // alignItems: 'center',
-    justifyContent: "flex-start",
-    margin: 5,
+    // justifyContent: "flex-start",
     backgroundColor: "lightgrey",
+    marginTop: 25,
   },
   cartContainer: {
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: 'orange',
+    padding: 10,
+    marginHorizontal: 20,
+    marginTop: 25,
+    marginBottom: 10,
+    borderRadius: 10
   },
   cartText: {
     color: "white",
     fontWeight: "600",
+    fontSize: 16
   },
 });
 
